064894f15efa14dc8eeddc83c699552dc47e4aca
Dominik Dorčák <dominik.dorcak@gmail.com>
2024-10-10 20:02:30 +0200

    add static params

--- src/app/list/[listId]/item/[itemId]/page.tsx
+++ src/app/list/[listId]/item/[itemId]/page.tsx
@@ -5,6 +5,9 @@ import React from "react";
 import {queryClient} from "@/app/QueryClient";
 import ItemForm from "@/components/TodoItem/ItemForm";
 
+export function generateStaticParams() {
+    return [{ listId: "1", itemId: "1" }]
+}
 
 export default function Page({params}: { params: { listId: string, itemId: string } }) {
     return (
